a65d9ef71ec1bcff8287c0f52f1151d44ab374f6
kondy <51909685+2739673455@users.noreply.github.com>
2023-03-30 22:03:35 +0800

    Add files via upload

--- pixiv搜索.py
+++ pixiv搜索.py
@@ -11,11 +11,13 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 class Thumbnail_Download_Thread(QThread): #下载预览图线程
-    id_sign = pyqtSignal(str)
+    num_sign = pyqtSignal(int)
+    img_download_url = pyqtSignal(str)
     textsig = pyqtSignal(str)
     def __init__(self,thumbnail_urls):
         super().__init__()
         self.thumbnail_urls = thumbnail_urls
+        self.num = 0
 
     def thumbnail_download(self,thumbnail_url):
         id=thumbnail_url.split('/')[-1].split('_')[0] #获取图片id
@@ -23,6 +25,8 @@ class Thumbnail_Download_Thread(QThread): #下载预览图线程
         img_url = 'https://www.pixiv.net/artworks/'+id
         resp = requests.get(img_url,headers=headers)
         viewcount = re.search('"viewCount":(.*?),',resp.text).group(1)
+        self.num+=1
+        self.num_sign.emit(int(self.num))
         if int(viewcount)>view:
             img_download_url = re.search('"original":"(.+?)"',resp.text).group(1)
             thumbnail_=requests.get(thumbnail_url,headers=headers,proxies=proxies)
@@ -32,7 +36,7 @@ class Thumbnail_Download_Thread(QThread): #下载预览图线程
                 with open(file,'wb') as f:
                     f.write(thumbnail_.content)
                 self.textsig.emit(f'{id}已下载,{viewcount}')
-            self.id_sign.emit(str(img_download_url))
+            self.img_download_url.emit(str(img_download_url))
 
     def run(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
@@ -61,8 +65,6 @@ class Pixiv(QDialog):
     def __init__(self):
         super().__init__()
         self.img_d_url_dict = dict()
-        self.thumbnail_urls = self.get_url(headers,page)
-        self.thumbnail_list = os.listdir(path_t)
         self.button_num = 0
         self.initUI()
     
@@ -71,24 +73,78 @@ class Pixiv(QDialog):
         self.setWindowTitle("Pixiv_搜索")
         self.resize(1600,1200)
         self.layout1 = QVBoxLayout()
-
+        self.layout2 = QHBoxLayout()
+        self.layout3 = QHBoxLayout()
+        self.layout4 = QVBoxLayout()
+        self.label1 = QLabel('搜索内容')
+        self.label2 = QLabel('最低浏览量')
+        self.label3 = QLabel('页码')
+        self.lineedit1 = QLineEdit(target)
+        self.lineedit2 = QLineEdit(str(view))
+        self.spinbox1 = QSpinBox()
+        self.spinbox1.setValue(page)
         self.scrollarea = QScrollArea(self)
         self.win = QWidget()
         self.grid = QGridLayout()
-
         self.textbrowser1 = QTextBrowser()
-        self.textbrowser1.setFixedSize(1500,200)
-
-        self.layout1.addWidget(self.scrollarea)
-        self.layout1.addWidget(self.textbrowser1)
-        self.setLayout(self.layout1)
-
+        self.textbrowser1.setFixedSize(1000,200)
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setFixedSize(300, 30)
+        self.button2 = QPushButton('搜索')
+        self.button2.setFixedSize(150, 50)
+        self.button2.clicked.connect(self.start)
+        self.button3 = QPushButton('清空')
+        self.button3.setFixedSize(150, 50)
+        self.button3.clicked.connect(self.clear)
         self.win.setLayout(self.grid)
         self.scrollarea.setWidget(self.win)
+        self.layout1.addLayout(self.layout2)
+        self.layout1.addWidget(self.scrollarea)
+        self.layout1.addLayout(self.layout3)
+        self.layout2.addWidget(self.label1)
+        self.layout2.addWidget(self.lineedit1)
+        self.layout2.addWidget(self.label2)
+        self.layout2.addWidget(self.lineedit2)
+        self.layout2.addWidget(self.label3)
+        self.layout2.addWidget(self.spinbox1)
+        self.layout3.addWidget(self.textbrowser1)
+        self.layout3.addLayout(self.layout4)
+        self.layout4.addWidget(self.progress_bar)
+        self.layout4.addWidget(self.button2)
+        self.layout4.addWidget(self.button3)
+        self.layout4.setAlignment(self.progress_bar, Qt.AlignCenter)
+        self.layout4.setAlignment(self.button2, Qt.AlignCenter)
+        self.layout4.setAlignment(self.button3, Qt.AlignCenter)
+        self.setLayout(self.layout1)
+
+    def start(self):
+        global target,target1,view,page,path_t,path_i
+        self.scrollarea.setWidgetResizable(True)
+        target = self.lineedit1.text()
+        target1 = urllib.parse.quote(target)
+        view = int(self.lineedit2.text())
+        page = self.spinbox1.value()
+        path_t = 'D:/Pixiv_picture/'+target+'/thumbnail/'
+        path_i = 'D:/Pixiv_picture/'+target+'/image/'
+        if os.path.isdir(path_t)==False:
+            os.makedirs(path_t)
+        if os.path.isdir(path_i)==False:
+            os.mkdir(path_i)
+        self.thumbnail_urls = self.get_url(headers,page)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setMaximum(len(self.thumbnail_urls))
         self.thumbnail_download_thread = Thumbnail_Download_Thread(self.thumbnail_urls)
-        self.thumbnail_download_thread.id_sign.connect(self.addimgbutton)
+        self.thumbnail_download_thread.num_sign.connect(self.refresh_progress_bar)
+        self.thumbnail_download_thread.img_download_url.connect(self.addimgbutton)
         self.thumbnail_download_thread.textsig.connect(self.append_text)
         self.thumbnail_download_thread.start()
+    
+    def clear(self):
+        self.button_num = 0
+        while self.grid.count():
+            child = self.grid.takeAt(0)
+            if child.widget():
+                child.widget().deleteLater()
 
     def addimgbutton(self,img_download_url): #添加按钮
         id = img_download_url.split('/')[-1].split('_')[0]
@@ -117,25 +173,22 @@ class Pixiv(QDialog):
 
     def append_text(self, text):
         self.textbrowser1.append(text)
+    
+    def refresh_progress_bar(self,value):
+        self.progress_bar.setValue(value)
 
 if __name__ == "__main__":
     urllib3.disable_warnings()
-    target = '温迪'
-    page = 1
-    view = 100
-    path_t = 'D:/'+target+'/thumbnail/'
-    path_i = 'D:/'+target+'/image/'
+    target = '原神'
     target1 = urllib.parse.quote(target)
-    if os.path.isdir(path_t)==False:
-        os.makedirs(path_t)
-    if os.path.isdir(path_i)==False:
-        os.mkdir(path_i)
+    view = 100
+    page = 1
+    path_t = 'D:/Pixiv_picture/'+target+'/thumbnail/'
+    path_i = 'D:/Pixiv_picture/'+target+'/image/'
     proxies={'https':'127.0.0.1:7890'}
-    headers={
-    'Connection':'close',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    'cookie': 'first_visit_datetime_pc=2022-12-15+20:36:48; p_ab_id=3; p_ab_id_2=7; p_ab_d_id=815045519; yuid_b=QHdmlZM; __utmz=235335808.1671104212.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=25093650_mhfz1BbsvI13yOlSjF4tylzwfPtP6gmS; device_token=3fad70a5ea88a5cd26b7010a4b0b2c7b; privacy_policy_agreement=5; _ga_MZ1NL4PHH0=GS1.1.1671104213.1.0.1671104216.0.0.0; c_type=32; privacy_policy_notification=0; a_type=1; b_type=1; __utmv=235335808.|2=login ever=no=1^3=plan=normal=1^5=gender=male=1^6=user_id=25093650=1^9=p_ab_id=3=1^10=p_ab_id_2=7=1^11=lang=zh=1; _fbp=fb.1.1671104216530.1386501833; QSI_S_ZN_5hF4My7Ad6VNNAi=v:0:0; _im_vid=01GMAS5JTHACVBKHHJVG4AJDJW; _im_uid.3929=h.e6658c04c1308c0c; adr_id=uTbg186QFFz1PjBL5xPZ11nRgV2rEY9Ck4baiyMbporOqzZU; tag_view_ranking=0xsDLqCEW6~_EOd7bsGyl~5oPIfUbtd6~txZ9z5ByU7~cb-9gnu4GK~RTJMXD26Ak~c7QmKEJ54V~faHcYIP1U0~WVrsHleeCL~BSlt10mdnm~CiSfl_AE0h~6293srEnwa~jy1Ljjlbd1~kVBgVYqL2q~RxQtPoROWu~tN9MyonktM~ijb1Y04gkD~rT9WTjeTAD~PTyxATIsK0~dXoVfWNqZx~0Sds1vVNKR~d9UpgqVAEz~-sp-9oh8uv~Ltq1hgLZe3~Bd2L9ZBE8q~UcTTPypj4n~E1yS6jImG-~dDxAe02g2b~x5Jrzac4QC~U02eb9uf9U~pzZvureUki~Cac_6jhDcg~DADQycFGB0~QaiOjmwQnI~zvgW9BzK6h; cto_bundle=l5O99F9uakRXbVphSWR0JTJCWVAxRTUwSHNDYllJNkR2R0dwaUJCMTQ1cFhLc01ncmNYMnBaJTJCc1Q0Q1BCQnVCaWxKbEVXT1JrNFZnJTJCRGJvbWptUVhZTHhEeDQ4JTJCQU8lMkJSbUhtT3lIeXFvbGpBd1ZCJTJGR09EQ2pCVEFIMUdNOGVVcWRzdEdESmlMSzY5RTJLbzJCM05uempydSUyQm1JQSUzRCUzRA; _ga=GA1.1.696882263.1671104211; __utma=235335808.696882263.1671104211.1671104212.1671199686.2; __utmc=235335808; __utmt=1; __utmb=235335808.2.10.1671199686; __cf_bm=0eKBagUPrTdLqVHt1hNX43ToZMarQARnjlJguflh.9I-1671200031-0-AThTWk0iHwrmyySKi6Zl/RMeAPMo4UQR5Rrz+lhkwOkCSLI8MJlTwpD/fFaaPjQEIiHBiEAnNuqu0AUgWp7goMFWcShZRzUogswAi1dNRCsOnasKMNtik+UBNZ+wfv/3NDJtJxcpBefAoapSXeDZ1tl8Vq45DVn2D06WhO6u22Rn2pR3ZTjFxgxGOA96yKz0Vd1k54RxonJAAVrLftCj8Ew=; _ga_75BBYNYN9J=GS1.1.1671198837.2.1.1671200053.0.0.0',
-    # 'referer': f'{url}'
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54',
+        'cookie':'first_visit_datetime_pc=2023-03-22+23:46:27; p_ab_id=7; p_ab_id_2=7; p_ab_d_id=268037037; yuid_b=JSeGdIU; device_token=fc859ce3cccabbb0c975e25e6645c7b2; privacy_policy_agreement=5; privacy_policy_notification=0; a_type=1; b_type=1; _im_vid=01GW4WJSDKZJ6JJ90AZCF9QWRA; _fbp=fb.1.1679496412837.1254382210; login_ever=yes; _gcl_au=1.1.1444779397.1679496511; QSI_S_ZN_5hF4My7Ad6VNNAi=v:0:0; _gid=GA1.2.1701354189.1680181696; __cf_bm=4ApWFpNb5.gCF4sgmpJnzYLH_0SpnGsk8jifXJKzbDQ-1680183694-0-Adyrj7LQcfE4E5Qf5PeoU8bxq6xHOP3mYTtlXsnlx8qD/0JcF/UbVDlZlc7MAQKwBXPeoAaB5bV24JcEGZ6vWWQ5M6me6bfkplhIb5gLBROyNHMeAVkrziew7GQ1Fd1fXR6u7IRe9k/8muxbGtnzY10Rnit7bsV9ZJ4UfMbhA2AdG+xsNaGJfDBF4tSmGKeCaw==; tag_view_ranking=_EOd7bsGyl~0xsDLqCEW6~rRSKPKi9Dv~npWJIbJroU~VRuBtwFc6O~UC88m8Ncjp~t2ErccCFR9~liM64qjhwQ~UniH1CHOF_~j2Cs25NHKk~FXm3-0izvb~ziiAzr_h04~cb-9gnu4GK~b_rY80S-DW~DvbaBUMbjP~dqqWNpq7ul~9JUl9VZvAE~eusCd1AMhu~veODt1WdzX~2GVngxkxTb~59dAqNEUGJ~XhOHJMaDOw~zu--HBEX2K~gf2RRY4mSK~gyXGHTTt7f~tDad2ZQ_GJ~Lt-oEicbBr~TqiZfKmSCg~CkDjyRo6Vc~ZnVEmREUi7~PKZyEOllwO~EjFGSeP9su~KMpT0re7Sq~PFIXHSJMRA~7dpqkQl8TH~7YNsdVv1xN~KacCFmbZiS~azESOjmQSV~5oPIfUbtd6~qLRlQPULHt~fv-doZT2yO~1yo_FtLsad~FE9s9RlCMY~eVxus64GZU~0unkWsk4kG~JrQgdjRZtN~sGOT44B_bo~_zH3D1CHig~r01unnQL0a~5U2rd7nRim~JWOyXSsjO2~faHcYIP1U0~RTJMXD26Ak~rOnsP2Q5UN~TWrozby2UO~KN7uxuR89w~NHNBwIWrH_~lkoWqucyTw~e2syg_rHyV~6293srEnwa~FdBF-J6Pun~VZAPYEMQnQ~m7Ok4YJ8uN~YKP81wwPPT~vUJKYVMOoy~fD8UdX152A~fTz375i7dZ~aPdvNeJ_XM~OgLi_QXWK2~c0RwjBwJgL~MfZWI3Zuez~aWbv1jixtU~eYfNGKUJaM~Ltq1hgLZe3~Te-Gu2wMLd~ByLJlI4pD7~sdPX4119UR~GNcgbuT3T-~NAVlUJuHDQ~4TDL3X7bV9~6RcLf9BQ-w~LtW-gO6CmS~oJAJo4VO5E~ouiK2OKQ-A~HBlflqJjBZ~j0lptk7ELi~KyFwW1QnOO~svvkfO5_78~Ob2dVjBvWQ~7TL10-HUQU~ivS9O2ycg-~1s4b4irzBH~lk1y5y0FPV~Kl6iavssH9~LJo91uBPz4~kOe05XkXBp~3SAZKPd9Ah~_HnoAesBlN~VmlVgqGndA~CiSfl_AE0h; cto_bundle=S4vbhF9nbW50SiUyRlRtd1dBd25oJTJGSUlHJTJCVHFPOTVKVGllejI3NmlKUHN2UHVIeWolMkZ1THFDaFo1akc2NmpDbUZuZzY0RDBYclkyRWVsT1JQTER4eWs0V0hVd2VSWDZsM2hxUURCZlBDZnMzUjNmdEJraHkwWXUlMkZTWjNLZ1NQOXN5bWhPVlE2TjVROWc1dDlPdWdzb0hOSzhsVWlBJTNEJTNE; _ga_MZ1NL4PHH0=GS1.1.1680183742.3.1.1680183777.0.0.0; c_type=25; _gat_UA-1830249-3=1; PHPSESSID=92669508_X03OFCkI3e65wNenaDtXqhYoUyh80ESa; _ga=GA1.2.811011881.1679496389; _ga_75BBYNYN9J=GS1.1.1680181691.7.1.1680183840.0.0.0',
     }
     app = QApplication(sys.argv)
     w=Pixiv()
