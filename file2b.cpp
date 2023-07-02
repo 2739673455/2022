d7ec6dcfaf2650b06cdb528666e4908ad6e87256
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-02 20:13:06 +0800

    Add files via upload

---
+++ file2b.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<cstdio>
+#include<sstream>
+
+using namespace std;
+
+vector<string> stringSplit(const string &file1)
+{
+	vector<string> name_vector;
+	stringstream ss(file1);	
+	string item;
+	while(getline(ss, item, '.'))
+	{
+		name_vector.push_back(item);
+	}
+	return name_vector;
+}
+
+void file2b(const string &file1)
+{
+    int c;
+	stringstream file2;
+//    char file2[L_tmpnam] = {'\0'};
+//    tmpnam(file2);
+	vector<string> name_vector = stringSplit(file1);
+	file2<<name_vector[0]<<".dat";
+
+    ifstream filein(file1, ios_base::in | ios_base::binary); 
+	ofstream fileout(file2.str(), ios_base::out | ios_base::binary);
+	filein.seekg(ios::beg);
+    while ((c=filein.get())!=EOF) 
+    {
+		fileout<<c<<" ";
+    }
+    filein.close();
+	fileout.close();
+}
+
+int main(int argc, char* argv[])
+{
+    for (int i=1; i<argc; ++i)
+    {
+		file2b(argv[i]);
+    }
+
+    return 0;
+}
