40b50ede189c372f9665907dd1f28c9386cf9557
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-07 20:30:08 +0800

    Update Rename.cpp

--- Rename.cpp
+++ Rename.cpp
@@ -12,7 +12,7 @@ vector<string> getFiles(const string& self_name)
 	vector<string> files;
 	for (const auto& file_path : fs::recursive_directory_iterator(current_dir))
 	{
-		if (!(file_path.is_directory()) and file_path.path().string() != self_name)
+		if (!(file_path.is_directory()) and (file_path.path().string() != self_name) and (file_path.path().filename().string() != "Unlock.exe"))
 		{
 			files.push_back(file_path.path().string());
 		}
@@ -20,11 +20,11 @@ vector<string> getFiles(const string& self_name)
 	return files;
 }
 
-void backRename(const string & source_file)
+void backRename(const string& source_file)
 {
 	int pos;
 	pos = source_file.rfind(".");
-	string dest_file = source_file.substr(0,pos);
+	string dest_file = source_file.substr(0, pos);
 	rename(source_file.c_str(), dest_file.c_str());
 }
 
@@ -37,6 +37,8 @@ int main(int argc, char* argv[])
 	{
 		string temp = file + ".temp";
 		rename(file.c_str(), temp.c_str());
+		cmd = "Unlock.exe \"" + temp + "\"" + " \"" + file + "\"";
+		system(cmd.c_str());
 	}
 	return 0;
 }
