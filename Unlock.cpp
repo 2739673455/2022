7b00801cdcc3f5d5d430588535461a5c1d656c1a
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-07 20:06:24 +0800

    Add files via upload

--- Unlock.cpp
+++ Unlock.cpp
@@ -1,49 +1,12 @@
-#include <vector>
-#include <string>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
+﻿#include <iostream>
+#include<string>
 
-using namespace std;
-namespace fs = std::filesystem;
-
-vector<string> getAllFilesIncludeSubfolders(string path) {
-    vector<string> files;
-    for (const auto& entry : fs::recursive_directory_iterator(path)) {
-        if (!entry.is_directory()) {
-            files.push_back(entry.path().string());
-        }
-    }
-    return files;
-}
-
-void copyFile(string source, string dest) {
-    ifstream src(source, ios::binary);
-    ofstream dst(dest, ios::binary);
-    dst << src.rdbuf();
-}
-
-void renameFile(string source, string dest, string currentDir) {
-    //string cmd = "F:\\Unlock\\Unlock.exe";
-    string cmd = ".\\Unlock.exe";
-    cmd += " -sourcePath=\"" + source + "\" -destPath=\"" + dest + "\"";
-    cout << cmd << endl;
-    system(cmd.c_str());
-}
-
-int main(int argc, char* argv[]) {
-    string currentDir = fs::current_path().string();
-    string selfName = (fs::path(argv[0])).filename().string();
-    vector files = getAllFilesIncludeSubfolders(currentDir);
-    //for (string file : files) {
-    //    if (file.find(selfName) != string::npos || file.find("Unlock.exe") != string::npos) {
-    //        continue;
-    //    }
-    //    string temp = file + ".temp";
-    //    copyFile(file, temp);
-    //    fs::remove(file);
-    //    renameFile(temp, file, currentDir);
-    //}
-    system("pause");
+int main(int argc, char * argv[])
+{
+    using namespace std;
+    string source_file, dest_file;
+    source_file = argv[1];
+    dest_file = argv[2];
+    rename(source_file.c_str(), dest_file.c_str());
     return 0;
 }
