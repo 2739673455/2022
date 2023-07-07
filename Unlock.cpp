1fe75c87bd1675008206f2eab220d395a4c76525
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-07 20:11:35 +0800

    Update Unlock.cpp

--- Unlock.cpp
+++ Unlock.cpp
@@ -4,9 +4,8 @@
 int main(int argc, char * argv[])
 {
     using namespace std;
-    string source_file, dest_file;
-    source_file = argv[1];
-    dest_file = argv[2];
+    string source_file = argv[1];
+    string dest_file = argv[2];
     rename(source_file.c_str(), dest_file.c_str());
     return 0;
 }
