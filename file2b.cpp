50b75cc92122b01c43f033c5737548ab59e59351
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-08 19:23:56 +0800

    Update file2b.cpp

--- file2b.cpp
+++ file2b.cpp
@@ -3,55 +3,38 @@
 #include<string>
 #include<vector>
 #include<cstdio>
-#include<sstream>
 
 using namespace std;
 
-vector<string> stringSplit(const string &file1)
+void file2Binary(const string& source)
 {
-	vector<string> name_vector;
-	stringstream ss(file1);	
-	string item;
-	while(getline(ss, item, '.'))
-	{
-		name_vector.push_back(item);
-	}
-	return name_vector;
-}
+	int binary;
+	int pos = source.rfind(".");
+	string dest = source.substr(0, pos) + ".dat";
 
-void file2b(const string &file1)
-{
-    int c;
-	stringstream file2;
-	vector<string> name_vector = stringSplit(file1);
-	file2<<name_vector[0]<<".dat";
-//    char file2[L_tmpnam] = {'\0'};
-//    tmpnam(file2);
-
-    ifstream filein(file1, ios_base::in | ios_base::binary); 
-    ofstream fileout(file2.str(), ios_base::out);
-//    filein.seekg(4096);
+	ifstream filein(source, ios_base::in | ios_base::binary);
+	ofstream fileout(dest, ios_base::out);
+	//    filein.seekg(4096);
 	int column = 0;
-    while ((c=filein.get())!=EOF) 
-    {
+	while ((binary = filein.get()) != EOF)
+	{
 		if (column > 15)
 		{
 			column = 0;
-			fileout<<endl;
+			fileout << endl;
 		}
-	    fileout<<c<<" ";
+		fileout << binary << " ";
 		++column;
-    }
-    filein.close();
-    fileout.close();
+	}
+	filein.close();
+	fileout.close();
 }
 
 int main(int argc, char* argv[])
 {
-    for (int i=1; i<argc; ++i)
-    {
-		file2b(argv[i]);
-    }
-
-    return 0;
+	for (int i = 1; i < argc; ++i)
+	{
+		file2Binary(argv[i]);
+	}
+	return 0;
 }
