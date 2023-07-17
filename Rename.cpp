0942a9e450f727f5e5d5ead3c272b22551c907d4
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-17 16:44:34 +0800

    Update Rename.cpp

--- Rename.cpp
+++ Rename.cpp
@@ -2,6 +2,9 @@
 #include<vector>
 #include<iostream>
 #include<filesystem>
+#include<Windows.h>
+#include<thread>
+#include<mutex>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -20,30 +23,51 @@ vector<string> getFiles(const string& self_name)
 	return files;
 }
 
+void convert1(const vector<string> &files,const size_t num_files,int& num_convert1,mutex & mutex1)
+{
+	string temp_txt;
+	while (num_convert1 < num_files)
+	{
+		mutex1.lock();
+		++num_convert1;
+		temp_txt = files[num_convert1] + ".txt";
+		rename(files[num_convert1].c_str(), temp_txt.c_str());
+		mutex1.unlock();
+	}
+}
+
+void convert2(const vector<string> &files,const size_t num_files, int& num_convert2, int& num_convert1, mutex& mutex1)
+{
+	string temp_txt;
+	while (num_convert2 < num_files)
+	{
+		mutex1.lock();
+		if (num_convert2 > num_convert1)
+		{
+			mutex1.unlock();
+			return;
+		}
+		Sleep(100);
+		temp_txt = files[num_convert2] + ".txt";
+		rename(files[num_convert2].c_str(), temp_txt.c_str());
+		++num_convert2;
+		mutex1.unlock();
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	string self_name = fs::path(argv[0]).string();
 	vector<string> files = getFiles(self_name);
-	int feedback;
-	string cmd;
-	string temp_txt;
-	string temp_temp;
+	size_t num_files = files.size();
+	int num_convert1 = -1;
+	int num_convert2 = 0;
+	mutex mutex1;
 
-
-	for (string file : files)
-	{
-		temp_txt = file + ".txt";
-		cmd = "move \"" + file + "\"" + " \"" + temp_txt + "\" >nul";
-		system(cmd.c_str());
-	}
-
-	for (string file : files)
-	{
-		temp_txt = file + ".txt";
-		feedback = rename(temp_txt.c_str(), file.c_str());
-	}
-
-
+	thread convert_thread1(convert1, files, num_files, num_convert1, mutex1);
+	thread convert_thread2(convert2, files, num_files, num_convert2, num_convert1, mutex1);
+	convert_thread1.join();
+	convert_thread2.join();
 
 	////all
 	//for (string file : files)
