69326e7a3c169c692375a5e817cf422a079180e6
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-18 14:47:31 +0800

    Update unlock02.cpp

--- unlock02.cpp
+++ unlock02.cpp
@@ -7,47 +7,67 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-vector<string> getFiles(const string& self_name)
+class Solution
 {
-	string current_dir = fs::current_path().string();
-	vector<string> files;
-	for (const auto& file_path : fs::recursive_directory_iterator(current_dir))
+public:
+	Solution(string self_name)
 	{
-		if (!(file_path.is_directory()) and (file_path.path().string() != self_name) and (file_path.path().filename().string() != "ahh.exe"))
+		string current_dir = fs::current_path().string();
+		for (const auto& file_path : fs::recursive_directory_iterator(current_dir))
 		{
-			files.push_back(file_path.path().string());
+			if (!(file_path.is_directory()) and (file_path.path().string() != self_name))
+			{
+				m_files.push_back(file_path.path().string());
+			}
 		}
 	}
-	return files;
-}
+
+	void convert1()
+	{
+		for (string file : m_files)
+		{
+			m_temp1 = file + ".txt";
+			rename(file.c_str(), m_temp1.c_str());
+			cout << file << " convert 1" << endl;
+		}
+	}
+
+	void convert2()
+	{
+		Sleep(100);
+		for (string file : m_files)
+		{
+			m_temp1 = file + ".txt";
+			m_temp2 = file + ".temp";
+			m_cmd = "move \"" + m_temp1 + "\" \"" + m_temp2 + "\" >nul";
+			system(m_cmd.c_str());
+			cout << file << " convert 2" << endl;
+		}
+	}
+
+	void convert3()
+	{
+		Sleep(100);
+		for (string file : m_files)
+		{
+			m_temp2 = file + ".temp";
+			rename(m_temp2.c_str(), file.c_str());
+			cout << file << " convert 3" << endl;
+		}
+	}
+
+private:
+	vector<string> m_files;
+	string m_temp1;
+	string m_temp2;
+	string m_cmd;
+};
 
 int main(int argc, char* argv[])
 {
 	string self_name = fs::path(argv[0]).string();
-	vector<string> files = getFiles(self_name);
-	string cmd;
-	string temp1;
-	string temp2;
-	for (string file : files)
-	{
-		temp1 = file + ".txt";
-		rename(file.c_str(), temp1.c_str());
-		cout << file << "convert 1" << endl;
-	}
-	Sleep(100);
-	for (string file : files)
-	{
-		temp1 = file + ".txt";
-		temp2 = file + ".temp";
-		cmd = "move \"" + temp1 + "\" \"" + temp2 + "\" >nul";
-		system(cmd.c_str());
-		cout << file << "convert 2" << endl;
-	}
-	Sleep(100);
-	for (string file : files)
-	{
-		temp2 = file + ".temp";
-		rename(temp2.c_str(), file.c_str());
-		cout << file << "convert 3" << endl;
-	}
+	Solution s1(self_name);
+	s1.convert1();
+	s1.convert2();
+	s1.convert3();
 }
