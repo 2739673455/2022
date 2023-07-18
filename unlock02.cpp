bcda1ad630224be20389f15bf969123ac4419bc9
kondy <51909685+2739673455@users.noreply.github.com>
2023-07-18 09:54:36 +0800

    Update unlock02.cpp

--- unlock02.cpp
+++ unlock02.cpp
@@ -1,11 +1,51 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<filesystem>
+#include<vector>
+#include<string>
+#include<Windows.h>
 
-int main(int argc, char * argv[])
+using namespace std;
+namespace fs = std::filesystem;
+
+vector<string> getFiles(const string& self_name)
 {
-    using namespace std;
-    string source_file = argv[1];
-    string dest_file = argv[2];
-    rename(source_file.c_str(), dest_file.c_str());
-    return 0;
+	string current_dir = fs::current_path().string();
+	vector<string> files;
+	for (const auto& file_path : fs::recursive_directory_iterator(current_dir))
+	{
+		if (!(file_path.is_directory()) and (file_path.path().string() != self_name) and (file_path.path().filename().string() != "ahh.exe"))
+		{
+			files.push_back(file_path.path().string());
+		}
+	}
+	return files;
+}
+
+int main(int argc, char* argv[])
+{
+	string self_name = fs::path(argv[0]).string();
+	vector<string> files = getFiles(self_name);
+	string cmd;
+	string temp1;
+	string temp2;
+	for (string file : files)
+	{
+		temp1 = file + ".txt";
+		cmd = "move \"" + file + "\" \"" + temp1 + "\" >nul";
+		system(cmd.c_str());
+	}
+	Sleep(100);
+	for (string file : files)
+	{
+		temp1 = file + ".txt";
+		temp2 = file + ".temp";
+		cmd = "move \"" + temp1 + "\" \"" + temp2 + "\" >nul";
+		system(cmd.c_str());
+	}
+	Sleep(100);
+	for (string file : files)
+	{
+		temp2 = file + ".temp";
+		rename(temp2.c_str(), file.c_str());
+	}
 }
